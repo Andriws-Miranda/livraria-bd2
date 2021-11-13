78fb91acfb66efad1a316d334e171d6f83c3dc9b
matheushenriquelima <mateus.matheus.lima@gmail.com>
2021-11-12 22:48:18 -0300

    adiciona metodos sql de livraria e entidade

--- livraria/src/br/com/livraria/Main.java
+++ livraria/src/br/com/livraria/Main.java
@@ -1,18 +1,15 @@
 package br.com.livraria;
 
-import java.sql.Connection;
 import java.sql.SQLException;
+
+import br.com.livraria.teste.ConexaoTeste;
 
 public class Main {
 
-	public static void main(String[] args) {
-		try {
-			Connection con = Conexao.criarConexao();
-		} catch (ClassNotFoundException ce) {
-			ce.printStackTrace();
-		} catch (SQLException se) {
-			se.printStackTrace();
-		}
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		ConexaoTeste conexao = new ConexaoTeste();
+		conexao.executar();
 	}
 
 }
+
---
+++ livraria/src/br/com/livraria/configuracao/Conexao.java
@@ -0,0 +1,21 @@
+package br.com.livraria.configuracao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+	public static final String URL = "jdbc:postgresql://localhost:5432/livraria";
+	public static final String USER = "postgres";
+	public static final String PASSWORD = "admin";
+	
+	public static Connection criarConexao() throws ClassNotFoundException, SQLException {
+		Class.forName("org.postgresql.Driver");
+		Connection conecta = DriverManager.getConnection(URL, USER, PASSWORD);
+		if(conecta != null) {
+			System.out.println("Conexão realizada com sucesso! Bem vindo!");
+			return conecta;
+		}
+		return null;
+	}
+}
